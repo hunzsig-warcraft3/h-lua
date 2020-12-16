d41bccdb1107bc5c41ac589f6a70d1df118db133
xxqq9527 <18657277153@163.com>
2020-12-16 21:59:27 +0800

    新增给商店添加 删除出售物品

--- lib/unit.lua
+++ lib/unit.lua
@@ -745,3 +745,25 @@ end
 hunit.setFlyHeight = function(u, height, speed)
     cj.SetUnitFlyHeight(u, height, speed)
 end
+
+--- 给商店添加出售物品，只影响有出售物品的单位
+---@param whichUnit userdata
+---@param itemId number
+---@param currentStock number
+---@param stockMax number
+hunit.addItemShop = function(whichUnit, itemId, currentStock,stockMax)
+    if (type(itemId) == "string") then
+        itemId = string.char2id(itemId)
+    end
+    cj.AddItemToStock(whichUnit, itemId, currentStock,stockMax)
+end
+
+--- 删除商店物品
+---@param whichUnit userdata
+---@param itemId number
+hunit.delItemShop = function(whichUnit, itemId)
+    if (type(itemId) == "string") then
+        itemId = string.char2id(itemId)
+    end
+    cj.RemoveItemFromStock(whichUnit, itemId)
+end
